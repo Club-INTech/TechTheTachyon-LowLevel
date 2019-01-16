6bc2fba451e4c9e319b0f9527ffb96d8c619fa11
jglrxavpok <jglrxavpok@gmail.com>
2019-01-16 22:31:45 +0100

    Mode courant au lieu du mode tension

--- src/Sensors/SICKDT35_B15851.cpp
+++ src/Sensors/SICKDT35_B15851.cpp
@@ -10,10 +10,13 @@ SICKDT35_B15851::SICKDT35_B15851(uint8_t pin, uint16_t rangeMin, uint16_t rangeM
 }
 
 uint16_t SICKDT35_B15851::readDistance() {
-    uint16_t tension = (uint16_t) analogRead(pin);
-    double alpha = tension/1024.0;
-    Serial.printf("[DEBUG] >> %f (%i - %i)\n", alpha, rangeMin, rangeMax);
-    return (uint16_t ) (alpha*rangeMax + (1.0-alpha) * rangeMin);
+    uint16_t valueRead = (uint16_t) analogRead(pin);
+    double alpha = valueRead/1024.0;
+    double tension = alpha * 3.3;
+    tension -= 0.6; // le courant est entre 4 mA et 20 mA et la résistance est de 162 Ohm donc une tension minimale de 0.6V
+    double t = tension/(3.3-0.6);
+    Serial.printf("[DEBUG] >> %f (%i - %i)\n", t, rangeMin, rangeMax);
+    return (uint16_t ) (t*rangeMax + (1.0-t) * rangeMin);
 }
 
 void SICKDT35_B15851::setRange(uint16_t min, uint16_t max) {
